babe00d5551b3eb56d855128e6dad24b2c557f86
makiec7 <makiecada@gmail.com>
2016-12-06 09:00:54 +0100

    refactor in DB_connect.php

--- include/DB_connect.php
+++ include/DB_connect.php
@@ -1,7 +1,9 @@
 <?php
 	require_once 'include/config.php';
-	$conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
+	
+	$conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE); // open connection
 	if ($conn->connect_error) {
 		die("Connection failed: " . $conn->connect_error);
 	}
+	
 ?>
