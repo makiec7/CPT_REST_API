c2d4ec04b5f4e5fbc8c14966d19cfccb0080b152
makiec7 <makiecada@gmail.com>
2016-12-06 08:59:49 +0100

    refactoring in config.php

--- include/config.php
+++ include/config.php
@@ -1,10 +1,8 @@
 <?php
 header('Content-type: text/html; charset=utf-8');
-/**
- * Database config variables
- */
-define("DB_HOST", "sql7.freemysqlhosting.net");
-define("DB_USER", "sql7144346");
-define("DB_PASSWORD", "gaTmKQXHxm");
-define("DB_DATABASE", "sql7144346");
+
+define("DB_HOST", "sql7.freemysqlhosting.net"); // Host name
+define("DB_USER", "sql7144346"); 				// user name
+define("DB_PASSWORD", "gaTmKQXHxm"); 			// password
+define("DB_DATABASE", "sql7144346"); 			// database name 
 ?>
