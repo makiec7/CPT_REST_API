77d976898c9b8a9f488256b4269293e995964c0d
makiec7 <makiecada@gmail.com>
2016-12-06 09:06:32 +0100

    refactoring in delete_fault.php

--- delete_fault.php
+++ delete_fault.php
@@ -1,8 +1,11 @@
 <?php
 require_once 'include/DB_connect.php';
 
+// GET variables
 $id=$_GET['id'];
 
+
+// connecting
 $sql = "DELETE FROM cti_fault WHERE id='$id';";
 $result = $conn->query($sql);
 if ($result) {
